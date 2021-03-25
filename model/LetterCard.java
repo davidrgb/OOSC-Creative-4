4d98ebc2a1624e144efc8bd72f29873926ee53fe
davidrgb <davidrussellrgb@gmail.com>
2021-03-25 18:39:44 -0500

    Functional letter card rendering

--- model/LetterCard.java
+++ model/LetterCard.java
@@ -1,8 +1,12 @@
 package model;
 
+import java.awt.Font;
 import java.awt.Graphics2D;
 
 public class LetterCard extends Card implements ICardRender {
+
+    private final int X_OFFSET = 2 + 4;
+    private final int Y_OFFSET = 2 + 17;
 
     private String rank;
     
@@ -27,6 +31,8 @@ public class LetterCard extends Card implements ICardRender {
 
     @Override
     public void render(Graphics2D g2) {
-        //render letter card
+        super.render(g2);
+        g2.setFont(new Font("Courier New", Font.BOLD, 21));
+        g2.drawString(rank, X_OFFSET + getxLocation(), + Y_OFFSET + getyLocation());
     }
 }
