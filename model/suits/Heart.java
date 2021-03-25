cec575b6937232902dde6898334fc86c3f245eb5
davidrgb <davidrussellrgb@gmail.com>
2021-03-25 18:36:28 -0500

    Functional heart render

--- model/suits/Heart.java
+++ model/suits/Heart.java
@@ -8,6 +8,9 @@ public class Heart extends Suit implements ISuitRender {
     private final int X_OFFSET = 6;
     private final int Y_OFFSET = 6;
 
+    private final int TOP_RIGHT_OFFSET_X = 55;
+    private final int TOP_RIGHT_OFFSET_Y = 4;
+
     private final int BOUNDS = 20;
     private final int RADIUS = 6;
     private final int CIRCLE_OFFSET = 2;
@@ -19,20 +22,20 @@ public class Heart extends Suit implements ISuitRender {
     @Override
     public void render(Graphics2D g2) {
         g2.setColor(Color.RED);
-        g2.fillOval(xLocation + X_OFFSET - RADIUS, yLocation + Y_OFFSET - RADIUS, RADIUS * 2, RADIUS * 2);
-        g2.fillOval(xLocation + X_OFFSET - RADIUS + (RADIUS + CIRCLE_OFFSET), yLocation + Y_OFFSET - RADIUS, RADIUS * 2, RADIUS * 2);
+        g2.fillOval(xLocation + TOP_RIGHT_OFFSET_X + X_OFFSET - RADIUS, yLocation + TOP_RIGHT_OFFSET_Y + Y_OFFSET - RADIUS, RADIUS * 2, RADIUS * 2);
+        g2.fillOval(xLocation + TOP_RIGHT_OFFSET_X + X_OFFSET - RADIUS + (RADIUS + CIRCLE_OFFSET), yLocation + TOP_RIGHT_OFFSET_Y + Y_OFFSET - RADIUS, RADIUS * 2, RADIUS * 2);
 
         int[] xPoints = new int[3];
         int[] yPoints = new int[3];
 
-        xPoints[0] = xLocation + X_OFFSET - RADIUS;
-        yPoints[0] = yLocation + Y_OFFSET;
+        xPoints[0] = xLocation + TOP_RIGHT_OFFSET_X + X_OFFSET - RADIUS;
+        yPoints[0] = yLocation + TOP_RIGHT_OFFSET_Y + Y_OFFSET;
 
-        xPoints[1] = xLocation + X_OFFSET + BOUNDS - RADIUS;
-        yPoints[1] = yLocation + Y_OFFSET;
+        xPoints[1] = xLocation + TOP_RIGHT_OFFSET_X + X_OFFSET + BOUNDS - RADIUS;
+        yPoints[1] = yLocation + TOP_RIGHT_OFFSET_Y + Y_OFFSET;
 
-        xPoints[2] = xLocation + X_OFFSET + BOUNDS / 2 - RADIUS;
-        yPoints[2] = yLocation + Y_OFFSET + RADIUS + BOUNDS / 2;
+        xPoints[2] = xLocation + TOP_RIGHT_OFFSET_X + X_OFFSET + BOUNDS / 2 - RADIUS;
+        yPoints[2] = yLocation + TOP_RIGHT_OFFSET_Y + Y_OFFSET + RADIUS + BOUNDS / 2;
 
         g2.fillPolygon(xPoints, yPoints, xPoints.length);
     }
