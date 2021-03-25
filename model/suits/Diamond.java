e29bf58fdae98e69867d1d9b98c2aa46463466cf
davidrgb <davidrussellrgb@gmail.com>
2021-03-25 18:35:21 -0500

    Functional Diamond Render

--- model/suits/Diamond.java
+++ model/suits/Diamond.java
@@ -8,6 +8,9 @@ public class Diamond extends Suit implements ISuitRender {
     private final int X_OFFSET = 0;
     private final int Y_OFFSET = 0;
 
+    private final int TOP_RIGHT_OFFSET_X = 55;
+    private final int TOP_RIGHT_OFFSET_Y = 4;
+
     private final int MID_POINTS_OFFSET = 2;
 
     private final int BOUNDS = 20;
@@ -23,17 +26,17 @@ public class Diamond extends Suit implements ISuitRender {
         int[] xPoints = new int[4];
         int[] yPoints = new int[4];
 
-        xPoints[0] = xLocation + X_OFFSET + BOUNDS / 2;
-        yPoints[0] = yLocation + Y_OFFSET;
+        xPoints[0] = xLocation + TOP_RIGHT_OFFSET_X + X_OFFSET + BOUNDS / 2;
+        yPoints[0] = yLocation + TOP_RIGHT_OFFSET_Y + Y_OFFSET;
 
-        xPoints[1] = xLocation + X_OFFSET + BOUNDS - MID_POINTS_OFFSET;
-        yPoints[1] = yLocation + Y_OFFSET + BOUNDS / 2;
+        xPoints[1] = xLocation + TOP_RIGHT_OFFSET_X + X_OFFSET + BOUNDS - MID_POINTS_OFFSET;
+        yPoints[1] = yLocation + TOP_RIGHT_OFFSET_Y + Y_OFFSET + BOUNDS / 2;
 
-        xPoints[2] = xLocation + X_OFFSET + (BOUNDS / 2);
-        yPoints[2] = yLocation + Y_OFFSET + BOUNDS;
+        xPoints[2] = xLocation + TOP_RIGHT_OFFSET_X + X_OFFSET + (BOUNDS / 2);
+        yPoints[2] = yLocation + TOP_RIGHT_OFFSET_Y + Y_OFFSET + BOUNDS;
 
-        xPoints[3] = xLocation + X_OFFSET + MID_POINTS_OFFSET;
-        yPoints[3] = yLocation + Y_OFFSET + BOUNDS / 2;
+        xPoints[3] = xLocation + TOP_RIGHT_OFFSET_X + X_OFFSET + MID_POINTS_OFFSET;
+        yPoints[3] = yLocation + TOP_RIGHT_OFFSET_Y + Y_OFFSET + BOUNDS / 2;
 
         g2.fillPolygon(xPoints, yPoints, xPoints.length);
     }
