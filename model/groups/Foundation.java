d6536de735c2d918194ada30b8b8c750a70821a9
davidrgb <davidrussellrgb@gmail.com>
2021-03-25 20:13:01 -0500

    Functional foundation card rendering

--- model/groups/Foundation.java
+++ model/groups/Foundation.java
@@ -40,14 +40,23 @@ public class Foundation extends Group {
     }
 
     public void render(Graphics2D g2) {
+
+        int cardXLocation = xOffset + ((CARD_WIDTH + INNER_OFFSET));
+        int cardYLocation = Y_OFFSET;
+
         if (cards.size() == 0) {
-            int cardXLocation = xOffset + ((CARD_WIDTH + INNER_OFFSET));
-            int cardYLocation = Y_OFFSET;
             g2.setColor(Color.white);
             g2.fillRect(cardXLocation, cardYLocation, CARD_WIDTH, CARD_HEIGHT);
             g2.setColor(Color.black);
             g2.drawRect(cardXLocation, cardYLocation, CARD_WIDTH, CARD_HEIGHT); 
             suit.render(g2);
+        } else {
+            cards.get(cards.size() - 1).updateLocation(cardXLocation, cardYLocation);
+            g2.setColor(Color.white);
+            g2.fillRect(cardXLocation, cardYLocation, CARD_WIDTH, CARD_HEIGHT);
+            g2.setColor(Color.black);
+            g2.drawRect(cardXLocation, cardYLocation, CARD_WIDTH, CARD_HEIGHT); 
+            cards.get(cards.size() - 1).render(g2);
         }
     }
 
